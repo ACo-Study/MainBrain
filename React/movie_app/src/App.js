3f37aba97d9ded7ffa54ceac9a037fdc263d3161
Main-Brain <rlswnsehdehd@naver.com>
2019-11-05 00:12:45 +0900

    [React] 1일차

--- React/movie_app/src/App.js
+++ React/movie_app/src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
 
-function App() {
+const frontPath = "https://movie.naver.com/movie/bi/mi/basic.nhn?code=";
+const movieList = [
+  {
+    id: 1,
+    name: "82년생 김지영",
+    path: "179482",
+    rating: 1
+  },
+  {
+    id: 2,
+    name: "터미네이터: 다크 페이트",
+    path: "167605",
+    rating: 2
+  },
+  {
+    id: 3,
+    name: "날씨의 아이",
+    path: "181114",
+    rating: 3
+  },
+  {
+    id: 4,
+    name: "조커",
+    path: "167613",
+    rating: 4
+  },
+  {
+    id: 5,
+    name: "말레피센트 2",
+    path: "167635",
+    rating: 5
+  }
+];
+
+function Movie({ name, path, rating }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2><a href={path}>{name}</a></h2>
+      <h4>{rating}</h4>
     </div>
   );
+}
+
+Movie.propTypes = {
+  name: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
+
+function renderMovie(movies) {
+  console.log(movies);
+  return <Movie key={movies.id} name={movies.name} path={frontPath + movies.path} rating={movies.rating} />;
+}
+
+function App() {
+  return <div>{movieList.map(renderMovie)}</div>
 }
 
 export default App;
