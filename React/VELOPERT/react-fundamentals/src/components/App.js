b1b01e0b36050bc258ac3387a6d0556231dc17ba
Main-Brain <rlswnsehdehd@naver.com>
2019-11-11 22:07:44 +0900

    [React] 주소록 3

--- React/VELOPERT/react-fundamentals/src/components/App.js
+++ React/VELOPERT/react-fundamentals/src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Contact from './Contact';
+
 
 class App extends React.Component {
     render() {
         return (
-            <h1>Hello</h1>
+            <Contact />
         );
     }
 }
