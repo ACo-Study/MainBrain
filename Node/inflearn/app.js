b3927594d72c14690db1c0b453a2fdbfc0ada31b
Main-Brain <rlswnsehdehd@naver.com>
2019-11-11 22:42:11 +0900

    [Node] Routing 리펙토링

--- Node/inflearn/app.js
+++ Node/inflearn/app.js
@@ -1,18 +1,8 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const router = require('./router/index')
 const cors = require('cors')
-const mysql = require('mysql')
-
-var connection = mysql.createConnection({
-    host : 'localhost',
-    port : '3300',
-    user : 'root',
-    password : 'root',
-    database : 'jsman'
-})
-
-connection.connect()
 
 app.listen(3000, () => {
     console.log("start!! express server on port 3000");
@@ -20,37 +10,7 @@ app.listen(3000, () => {
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
-app.use(cors())
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/public/main.html");
-});
-
-app.get('/main', (req, res) => {
-    res.sendFile(__dirname + "/public/main.html");
-});
-
-app.post('/email_post', (req, res) => {
-    console.log(req.body.email)
-    // res.send("<h1>welcome " + req.body.email + "</h1>")
-    res.render('email.ejs', {'email' : req.body.email})
-})
-
-app.post('/ajax_send_email', (req, res) => {
-    var email = req.body.email;
-    var responseData = {};
-
-    var query = connection.query(`select name from user where email = '${email}'`, (err, rows) => {
-        if(err) throw err;
-        if(rows[0]) {
-            responseData.result = "ok";
-            responseData.name = rows[0].name
-        } else {
-            responseData.result = "none";
-            responseData.name = rows[0].name
-        }
-        res.json(responseData)
-    })
-})
+app.use(cors());
+app.use(router)
---
+++ Node/inflearn/router/email.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const router = express.Router();
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3300',
+    user: 'root',
+    password: 'root',
+    database: 'jsman'
+})
+connection.connect()
+
+router.post('/form', (req, res) => {
+    console.log(req.body.email)
+    // res.send("<h1>welcome " + req.body.email + "</h1>")
+    res.render('email.ejs', { 'email': req.body.email })
+})
+
+router.post('/ajax', (req, res) => {
+    var email = req.body.email;
+    var responseData = {};
+
+    var query = connection.query(`select name from user where email = '${email}'`, (err, rows) => {
+        if (err) throw err;
+        if (rows[0]) {
+            responseData.result = "ok";
+            responseData.name = rows[0].name;
+        } else {
+            responseData.result = "none";
+            responseData.name = "";
+        }
+        res.json(responseData)
+    })
+})
+
+module.exports = router;
