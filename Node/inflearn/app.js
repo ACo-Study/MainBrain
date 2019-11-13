e3e1b7518d65a9961ba2f4e0c47511dde24c042e
Main-Brain <rlswnsehdehd@naver.com>
2019-11-13 22:22:33 +0900

    [Node] passport-local

--- Node/inflearn/app.js
+++ Node/inflearn/app.js
@@ -1,16 +1,32 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const router = require('./router/index')
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const router = require("./router/index");
+const cors = require("cors");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+const flash = require("connect-flash");
 
 app.listen(3000, () => {
-    console.log("start!! express server on port 3000");
+  console.log("start!! express server on port 3000");
 });
 
-app.use(express.static('public'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.set('view engine', 'ejs')
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
 app.use(cors());
-app.use(router)
+app.use(router);
--- Node/inflearn/router/join.js
+++ Node/inflearn/router/join.js
@@ -3,6 +3,8 @@ const app = express();
 const mysql = require("mysql");
 const router = express.Router();
 const path = require("path");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -14,22 +16,45 @@ const connection = mysql.createConnection({
 connection.connect();
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/join.html"));
+  res.render("join.ejs");
 });
 
-router.post("/", (req, res) => {
-  const body = req.body;
-  const email = body.email;
-  const name = body.name;
-  const passwd = body.password;
-
-  const quert = connection.query(
-    `insert into user (email, name, pwd) values ('${email}','${name}','${passwd}')`,
-    (err, rows) => {
-      if (err) throw err;
-      else res.render("welcome.ejs", { name: name, id: rows.insertId });
+passport.use(
+  "local-join",
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+      passReqToCallBack: true
+    },
+    (req, email, password, done) => {
+      console.log("local-join callback called");
     }
-  );
-});
+  )
+);
+
+router.post(
+  "/",
+  passport.authenticate("local-join", {
+    successRedirect: "/main",
+    failurRedirect: "/join",
+    failureFlash: true
+  })
+);
+
+// router.post("/", (req, res) => {
+//   const body = req.body;
+//   const email = body.email;
+//   const name = body.name;
+//   const passwd = body.password;
+
+//   const quert = connection.query(
+//     `insert into user (email, name, pwd) values ('${email}','${name}','${passwd}')`,
+//     (err, rows) => {
+//       if (err) throw err;
+//       else res.render("welcome.ejs", { name: name, id: rows.insertId });
+//     }
+//   );
+// });
 
 module.exports = router;
