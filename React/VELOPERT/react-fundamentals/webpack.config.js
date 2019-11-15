0e054d9438d5f6a119cdd05811ded464ecb7e928
Main-Brain <rlswnsehdehd@naver.com>
2019-11-15 22:38:17 +0900

    [Project] reset

--- React/VELOPERT/react-fundamentals/webpack.config.js
+++ React/VELOPERT/react-fundamentals/webpack.config.js
@@ -1,35 +1,28 @@
-var webpack = require('webpack');
+var webpack = require("webpack");
 
 module.exports = {
-    entry: ['react-hot-loader/patch', './src/index.js'],
+  entry: "./src/index.js",
 
-    output: {
-        path: __dirname + '/public/',
-        filename: 'bundle.js'
-    },
+  output: {
+    path: __dirname + "/public/",
+    filename: "bundle.js"
+  },
 
-    devServer: {
-        hot: true,
-        inline: true,
-        host: '0.0.0.0',
-        port: 4000,
-        contentBase: __dirname + '/public/',
-    },
-
-    module: {
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
         loaders: [
-            {
-                test: /\.js$/,
-                loaders: ['react-hot-loader/webpack', 'babel-loader?' + JSON.stringify({
-                    cacheDirectory: true,
-                    presets: ["@babel/preset-env", "@babel/preset-react"]
-                })],
-                exclude: /node_modules/
-            }
-        ]
-    },
-
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
+          "babel-loader?" +
+            JSON.stringify({
+              cacheDirectory: true,
+              presets: ["@babel/preset-env", "@babel/preset-react"]
+            })
+        ],
+        exclude: /node_modules/
+      }
     ]
+  },
+
+  plugins: [new webpack.HotModuleReplacementPlugin()]
 };
