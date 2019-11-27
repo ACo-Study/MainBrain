1bea88420245dd53ea3025443497eac316c896a3
Main-Brain <rlswnsehdehd@naver.com>
2019-11-27 22:56:37 +0900

    [React] 주석 수정

--- React/VELOPERT/react-codelab-project-master/src/actions/authentication.js
+++ React/VELOPERT/react-codelab-project-master/src/actions/authentication.js
@@ -10,10 +10,24 @@ import {
   AUTH_GET_STATUS_FAILURE,
   AUTH_LOGOUT
 } from "./ActionTypes";
+import axios from "axios";
+
+/* ====== AUTH ====== */
 
 /* LOGIN */
 export function loginRequest(username, password) {
-  /* To be implemented */
+  return dispatch => {
+    dispatch(login());
+
+    return axios
+      .post("/api/account/signin", { username, password })
+      .then(response => {
+        dispatch(loginSuccess(username));
+      })
+      .catch(error => {
+        dispatch(loginFailure());
+      });
+  };
 }
 
 export function login() {
@@ -35,24 +49,15 @@ export function loginFailure() {
   };
 }
 
-/* Logout */
-export function logoutRequest() {
-  return dispatch => {
-    return axios.post("/api/account/logout").then(response => {
-      dispatch(logout());
-    });
-  };
-}
-
 /* REGISTER */
 export function registerRequest(username, password) {
   return dispatch => {
-    // Inform Register API is starting
+    // inform register API is starting
     dispatch(register());
 
     return axios
       .post("/api/account/signup", { username, password })
-      .then(response => {
+      .then(reponse => {
         dispatch(registerSuccess());
       })
       .catch(error => {
@@ -81,13 +86,12 @@ export function registerFailure(error) {
 }
 
 /* GET STATUS */
+
 export function getStatusRequest() {
   return dispatch => {
-    // inform Get Status API is starting
     dispatch(getStatus());
-
     return axios
-      .get("/api/account/getInfo")
+      .get("/api/account/getinfo")
       .then(response => {
         dispatch(getStatusSuccess(response.data.info.username));
       })
@@ -116,140 +120,17 @@ export function getStatusFailure() {
   };
 }
 
+/* LOGOUT */
+export function logoutRequest() {
+  return dispatch => {
+    return axios.post("/api/account/logout").then(response => {
+      dispatch(logout());
+    });
+  };
+}
+
 export function logout() {
   return {
     type: AUTH_LOGOUT
   };
 }
-
-// import {
-//     AUTH_LOGIN,
-//     AUTH_LOGIN_SUCCESS,
-//     AUTH_LOGIN_FAILURE,
-//     AUTH_REGISTER,
-//     AUTH_REGISTER_SUCCESS,
-//     AUTH_REGISTER_FAILURE,
-//     AUTH_GET_STATUS,
-//     AUTH_GET_STATUS_SUCCESS,
-//     AUTH_GET_STATUS_FAILURE,
-//     AUTH_LOGOUT
-// } from './ActionTypes';
-// import axios from 'axios';
-
-// /* ====== AUTH ====== */
-
-// /* LOGIN */
-// export function loginRequest(username, password) {
-//     return (dispatch) => {
-//             dispatch(login());
-
-//             return axios.post('/api/account/signin', { username, password })
-//             .then((response) => {
-//                 dispatch(loginSuccess(username));
-//             }).catch((error) => {
-//                 dispatch(loginFailure());
-//             });
-//     };
-// }
-
-// export function login() {
-//     return {
-//         type: AUTH_LOGIN
-//     };
-// }
-
-// export function loginSuccess(username) {
-//     return {
-//         type: AUTH_LOGIN_SUCCESS,
-//         username
-//     };
-// }
-
-// export function loginFailure() {
-//     return {
-//         type: AUTH_LOGIN_FAILURE
-//     };
-// }
-
-// /* REGISTER */
-// export function registerRequest(username, password) {
-//     return (dispatch) => {
-//         // inform register API is starting
-//         dispatch(register());
-
-//         return axios.post('/api/account/signup', { username, password })
-//         .then((reponse) => {
-//             dispatch(registerSuccess());
-//         }).catch((error) => {
-//             dispatch(registerFailure(error.response.data.code));
-//         });
-//     };
-// }
-
-// export function register() {
-//     return {
-//         type: AUTH_REGISTER
-//     };
-// }
-
-// export function registerSuccess() {
-//     return {
-//         type: AUTH_REGISTER_SUCCESS
-//     };
-// }
-
-// export function registerFailure(error) {
-//     return {
-//         type: AUTH_REGISTER_FAILURE,
-//         error
-//     };
-// }
-
-// /* GET STATUS */
-
-// export function getStatusRequest() {
-//     return (dispatch) => {
-//         dispatch(getStatus());
-//         return axios.get('/api/account/getinfo')
-//         .then((response) => {
-//             dispatch(getStatusSuccess(response.data.info.username));
-//         }).catch((error) => {
-//             dispatch(getStatusFailure());
-//         });
-//     };
-// }
-
-// export function getStatus() {
-//     return {
-//         type: AUTH_GET_STATUS
-//     };
-// }
-
-// export function getStatusSuccess(username) {
-//     return {
-//         type: AUTH_GET_STATUS_SUCCESS,
-//         username
-//     };
-// }
-
-// export function getStatusFailure() {
-//     return {
-//         type: AUTH_GET_STATUS_FAILURE
-//     };
-// }
-
-// /* LOGOUT */
-// export function logoutRequest() {
-//     return (dispatch) => {
-//         return axios.post('/api/account/logout')
-//         .then((response) => {
-//             dispatch(logout());
-//         });
-//     };
-// }
-
-// export function logout() {
-//     return {
-//         type: AUTH_LOGOUT
-//     };
-// }
