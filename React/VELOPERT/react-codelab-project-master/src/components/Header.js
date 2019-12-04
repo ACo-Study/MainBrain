03402f9d343762a50c2d053f0cbc3903a7fd2bdc
Main-Brain <rlswnsehdehd@naver.com>
2019-12-04 22:35:03 +0900

    [React] 주석 수정

--- React/VELOPERT/react-codelab-project-master/src/components/Header.js
+++ React/VELOPERT/react-codelab-project-master/src/components/Header.js
@@ -1,20 +1,33 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router";
+import { Search } from "components";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
-class Header extends Component {
+class Header extends React.Component {
   constructor(props) {
     super(props);
+
+    // IMPLEMENT: CREATE A SEARCH STATUS
+
+    this.state = {
+      search: false
+    };
+
+    this.toggleSearch = this.toggleSearch.bind(this);
+  }
+
+  toggleSearch() {
+    this.setState({
+      search: !this.state.search
+    });
   }
 
   render() {
     const loginButton = (
       <li>
-        <a>
-          <Link to="/login">
-            <i className="material-icons">vpn_key</i>
-          </Link>
-        </a>
+        <Link to="/login">
+          <i className="material-icons">vpn_key</i>
+        </Link>
       </li>
     );
 
@@ -27,117 +40,59 @@ class Header extends Component {
     );
 
     return (
-      <nav>
-        <div className="nav-wrapper blue darken-1">
-          <t className="brand-logo center">MEMOPAD</t>
-          <ul>
-            <li>
-              <a>
-                <i className="material-icons">search</i>
-              </a>
-            </li>
-          </ul>
+      <div>
+        <nav>
+          <div className="nav-wrapper blue darken-1">
+            <Link to="/" className="brand-logo center">
+              MEMOPAD
+            </Link>
 
-          <div className="right">
-            <ul>{this.props.isLoggedIn ? logoutButton : loginButton}</ul>
+            <ul>
+              <li>
+                <a onClick={this.toggleSearch}>
+                  <i className="material-icons">search</i>
+                </a>
+              </li>
+            </ul>
+
+            <div className="right">
+              <ul>{this.props.isLoggedIn ? logoutButton : loginButton}</ul>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+        <ReactCSSTransitionGroup
+          transitionName="search"
+          transitionEnterTimeout={300}
+          transitionLeaveTimeout={300}
+        >
+          {/* IMPLEMENT: SHOW SEARCH WHEN SEARCH STATUS IS TRUE */}
+          {this.state.search ? (
+            <Search
+              onClose={this.toggleSearch}
+              onSearch={this.props.onSearch}
+              usernames={this.props.usernames}
+            />
+          ) : (
+            undefined
+          )}
+        </ReactCSSTransitionGroup>
+      </div>
     );
   }
 }
 
 Header.propTypes = {
-  isLoggedIn: PropTypes.bool,
-  onLogout: PropTypes.func
+  isLoggedIn: React.PropTypes.bool,
+  onLogout: React.PropTypes.func,
+  usernames: React.PropTypes.array
 };
 
 Header.defaultProps = {
   isLoggedIn: false,
   onLogout: () => {
     console.error("logout function not defined");
-  }
+  },
+  usernames: []
 };
 
 export default Header;
-
-// import React from 'react';
-// import { Link } from 'react-router';
-// import { Search } from 'components';
-// import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-
-// class Header extends React.Component {
-
-//     constructor(props) {
-//         super(props);
-
-//         // IMPLEMENT: CREATE A SEARCH STATUS
-
-//         this.state = {
-//             search: false
-//         };
-
-//         this.toggleSearch = this.toggleSearch.bind(this);
-//     }
-
-//     toggleSearch() {
-//         this.setState({
-//             search: !this.state.search
-//         });
-//     }
-
-//     render() {
-
-//         const loginButton = (
-//             <li>
-//                 <Link to="/login"><i className="material-icons">vpn_key</i></Link>
-//             </li>
-//         );
-
-//         const logoutButton = (
-//             <li>
-//                 <a onClick={this.props.onLogout}><i className="material-icons">lock_open</i></a>
-//             </li>
-//         );
-
-//         return (
-//             <div>
-//                 <nav>
-//                     <div className="nav-wrapper blue darken-1">
-//                         <Link to="/" className="brand-logo center">MEMOPAD</Link>
-
-//                         <ul>
-//                             <li><a onClick={this.toggleSearch}><i className="material-icons">search</i></a></li>
-//                         </ul>
-
-//                         <div className="right">
-//                             <ul>
-//                                 { this.props.isLoggedIn ? logoutButton : loginButton }
-//                             </ul>
-//                         </div>
-//                     </div>
-//                 </nav>
-//                     <ReactCSSTransitionGroup transitionName="search" transitionEnterTimeout={300} transitionLeaveTimeout={300}>
-//                         { /* IMPLEMENT: SHOW SEARCH WHEN SEARCH STATUS IS TRUE */}
-//                         {this.state.search ? <Search onClose={this.toggleSearch}
-//                         onSearch={this.props.onSearch}
-//                         usernames={this.props.usernames}/> : undefined }
-//                     </ReactCSSTransitionGroup>
-//             </div>
-//         );
-//     }
-// }
-
-// Header.propTypes = {
-//     isLoggedIn: React.PropTypes.bool,
-//     onLogout: React.PropTypes.func,
-//     usernames: React.PropTypes.array
-// };
-
-// Header.defaultProps = {
-//     isLoggedIn: false,
-//     onLogout: () => { console.error("logout function not defined");},
-//     usernames: []
-// };
-
-// export default Header;
