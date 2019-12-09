546ce8711f26c086c3174814b90649cc00c1543d
Main-Brain <rlswnsehdehd@naver.com>
2019-12-09 22:23:28 +0900

    주석 해제

--- React/VELOPERT/react-codelab-project-master/src/containers/Register.js
+++ React/VELOPERT/react-codelab-project-master/src/containers/Register.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Authentication } from "../components";
+import { Authentication } from "components";
 import { connect } from "react-redux";
-import { registerRequest } from "../actions/authentication";
+import { registerRequest } from "actions/authentication";
+import { browserHistory } from "react-router";
 
 class Register extends React.Component {
   constructor(props) {
@@ -12,16 +13,16 @@ class Register extends React.Component {
   handleRegister(id, pw) {
     return this.props.registerRequest(id, pw).then(() => {
       if (this.props.status === "SUCCESS") {
-        Materialize.toast("Success! Please log in.", 2000);
-        this.props.history.push("/login");
+        Materialize.toast("Success! Please log in", 2000);
+        browserHistory.push("/login");
         return true;
       } else {
         /*
-          ERROR CODES:
-            1: BAD USERNAME
-            2: BAD PASSWORD
-            3: USERNAME EXISTS
-        */
+                       ERROR CODES:
+                           1: BAD USERNAME
+                           2: BAD PASSWORD
+                           3: USERNAME EXISTS
+                   */
         let errorMessage = [
           "Invalid Username",
           "Password is too short",
@@ -38,7 +39,6 @@ class Register extends React.Component {
       }
     });
   }
-
   render() {
     return (
       <div>
@@ -64,69 +64,3 @@ const mapDispatchToProps = dispatch => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Register);
-
-// import React from 'react';
-// import { Authentication } from 'components';
-// import { connect } from 'react-redux';
-// import { registerRequest } from 'actions/authentication';
-// import { browserHistory } from 'react-router';
-
-// class Register extends React.Component {
-
-//     constructor(props) {
-//         super(props);
-//         this.handleRegister = this.handleRegister.bind(this);
-//     }
-
-//     handleRegister(id, pw) {
-//         return this.props.registerRequest(id, pw).then(
-//             () => {
-//                 if(this.props.status === "SUCCESS") {
-//                     Materialize.toast('Success! Please log in', 2000);
-//                     browserHistory.push('/login');
-//                     return true;
-//                 } else {
-//                     /*
-//                        ERROR CODES:
-//                            1: BAD USERNAME
-//                            2: BAD PASSWORD
-//                            3: USERNAME EXISTS
-//                    */
-//                    let errorMessage = [
-//                        'Invalid Username',
-//                        'Password is too short',
-//                        'Username already exists'
-//                    ];
-
-//                    let $toastContent = $('<span style="color: #FFB4BA">' + errorMessage[this.props.errorCode - 1] + '</span>');
-//                     Materialize.toast($toastContent, 2000);
-//                     return false;
-//                 }
-//             }
-//         );
-//     }
-//     render() {
-//         return (
-//             <div>
-//                 <Authentication mode={false} onRegister={this.handleRegister}/>
-//             </div>
-//         );
-//     }
-// }
-
-// const mapStateToProps = (state) => {
-//     return {
-//         status: state.authentication.register.status,
-//         errorCode: state.authentication.register.error
-//     };
-// };
-
-// const mapDispatchToProps = (dispatch) => {
-//     return {
-//         registerRequest: (id, pw) => {
-//             return dispatch(registerRequest(id,pw));
-//         }
-//     };
-// };
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Register);
